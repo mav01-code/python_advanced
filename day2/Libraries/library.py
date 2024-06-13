19dba40c4e6a1e50238372ece7f868533e22f4ac
mav01-code <marreddyakshayavarshini@gmail.com>
2024-06-13 11:52:56 +0530

    Libraries

---
+++ day2/Libraries/library.py
@@ -0,0 +1,6 @@
+# Collection of modules - library
+# Standard libraries - comes bundled with py, don't need to install it separately
+# Third party library - Not included in py, need to install with pip
+
+import django
+print(django.get_version())
--- day2/Modules/module.py
+++ day2/Modules/module.py
@@ -1,2 +1,16 @@
-import module1
-print(module1.greet("Akshaya Varshini"))
+# Method - 1
+# import module1
+# print(module1.greet("Akshaya Varshini"))
+
+# Method - 2
+# from module1 import greet
+# print(greet("Akshaya Varshini"))
+
+# Built-in modules - datetime, math
+
+# import datetime
+# now= datetime.datetime.now()
+# print(now)
+
+# import math
+# print(math.sqrt(16))
