2d8cb95ab99efde3832281b2cbba928c89f8f179
mav01-code <marreddyakshayavarshini@gmail.com>
2024-06-18 11:25:46 +0530

    Map

---
+++ day5/functions.py
@@ -0,0 +1,12 @@
+#Types of functions - (4) - Positional arg, default arguments, keyword argument, variable length argument
+
+# Default argument function
+"""
+def add(a=None, b=None):
+    if a is None or b is None:
+        return "Please provide values to calculate the sum"
+    return a+b
+
+res = add()
+print(res)
+"""
---
+++ day5/map.py
@@ -0,0 +1,24 @@
+"""
+def square(num):
+    return num*num
+data = [1, 2, 3, 4, 5]
+
+# squares = []
+# for i in data:
+#     squares.append(square(i))
+# print(squares)
+
+# Using map
+res = list(map(square, data)) # Syntax: map(function, iterables), iterable: list, tuple, etc...
+print(res)
+"""
+
+def verify(age):
+    # if age>18:          
+    #     return True
+    # else:
+    #     return False
+    return age>18
+ages = [12, 45, 23, 54, 13, 14]
+adult = list(filter(verify, ages))
+print(adult)
