b216b80cfbe7148832ec67a2afc6421353b94ea8
mav01-code <marreddyakshayavarshini@gmail.com>
2024-06-18 12:01:24 +0530

    lambda functions

---
+++ day5/lambda.py
@@ -0,0 +1,38 @@
+# Sorting a 2D array
+"""
+arr = [
+    [1,2,3,4,5],
+    [5,4,3,2,1],
+    [4,5,2,1,3],
+    [1,5,4,3,2],
+    [1,2,3,5,4]
+]
+
+# for a in arr:
+#     a.sort()
+# print(arr)
+
+arr.sort(key=lambda ele:ele[-1] )
+print(*arr, sep="\n")
+"""
+
+# from functools import reduce
+
+# def sum(a,b):
+#     return a+b
+# data=[1,2,3,4,5]
+# res = reduce(lambda a,b: a+b, data, 10)
+# print(res)
+
+# Factorial function using lambda and reduce functions
+"""
+from functools import reduce
+
+n = 5
+
+data=list(range(1, n+1))
+print(data)
+res = reduce(lambda a,b: a*b, data)
+print(res)
+"""
+
