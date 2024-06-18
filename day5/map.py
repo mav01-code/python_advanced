4f312656c041e4f606cc887b268500a6c42ba12e
mav01-code <marreddyakshayavarshini@gmail.com>
2024-06-18 11:36:44 +0530

    reduce function

--- day5/map.py
+++ day5/map.py
@@ -12,7 +12,7 @@ data = [1, 2, 3, 4, 5]
 res = list(map(square, data)) # Syntax: map(function, iterables), iterable: list, tuple, etc...
 print(res)
 """
-
+"""
 def verify(age):
     # if age>18:          
     #     return True
@@ -22,3 +22,14 @@ def verify(age):
 ages = [12, 45, 23, 54, 13, 14]
 adult = list(filter(verify, ages))
 print(adult)
+"""
+
+# Reduce function - takes  args - reduce(function, data)
+
+from functools import reduce
+def func(a, b):
+    return a+b
+
+data = [1,2,3,4,5]
+sum = reduce(func, data)
+print(sum)
