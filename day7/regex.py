f64d818ec101f15ba6d0f565b8f976d9aa635c33
mav01-code <marreddyakshayavarshini@gmail.com>
2024-06-20 11:37:03 +0530

    Verification using RegEx

--- day7/regex.py
+++ day7/regex.py
@@ -41,3 +41,65 @@ for row in res:
 
 # for row in res:
 #     print(row)
+
+#                   ------------------------- Email verification---------------------------
+"""
+import re
+
+file = open("D:\\python_advanced\\day7\\example.txt","r")
+data = file.read()
+pattern = re.compile( r"[a-zA-Z0-9]{10}@iare.ac.in" ) 
+res = re.finditer(pattern, data)
+
+for row in res:
+    print(row)
+"""
+
+"""
+import re
+
+file = open("D:\\python_advanced\\day7\\example.txt","r")
+data = file.read()
+pattern = re.compile( r"[a-zA-Z0-9-._]+@[a-z0-9]+\.[a-z.]+" ) 
+res = re.finditer(pattern, data)
+
+for row in res:
+    print(row)
+"""
+
+#                   ------------------------- Roll Number Verification---------------------------
+
+import re
+
+file = open("D:\\python_advanced\\day7\\example.txt","r")
+data = file.read()
+pattern = re.compile( r"[0-9]{2}[0-9]{2}[0-9]{1}[A-Z]{1}[0-9]{2}[A-Z0-9]{2}" ) 
+res = re.finditer(pattern, data)
+
+for row in res:
+    print(row)
+
+    #                   ------------------------- Name Verification---------------------------
+
+import re
+
+file = open("D:\\python_advanced\\day7\\example.txt","r")
+data = file.read()
+pattern = re.compile( r"(Mrs|Ms|Mr)[. ]*[a-zA-Z ]+" ) 
+res = re.finditer(pattern, data)
+
+for row in res:
+    print(row)
+
+
+   #                   ------------------------- Name Verification---------------------------
+
+import re
+
+file = open("D:\\python_advanced\\day7\\example.txt","r")
+data = file.read()
+pattern = re.compile( r"(?=[A-Z]+)(?=[a-z])(?=)" ) 
+res = re.finditer(pattern, data)
+
+for row in res:
+    print(row)
